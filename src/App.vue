5115f006909453068f53acc494b4cb668b4f1574
Minzhi <li.minzhi@dhs.sg>
2020-03-16 18:43:18 +0800

    Add firebase and Register Login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <proj-mates></proj-mates>
+   <!-- <proj-mates></proj-mates>  -->
     <login></login>
   </div>
 </template>
 
 <script>
-import ProjMates from './components/ProjMates.vue'
+// import ProjMates from './components/ProjMates.vue'
 import Login from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    'proj-mates' : ProjMates,
     'login' : Login
   }
 }
