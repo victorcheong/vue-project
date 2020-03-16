53a8e628a2ab5205fb9b235f87ddb23857f45eec
Minzhi <li.minzhi@dhs.sg>
2020-03-16 18:34:45 +0800

    Register login

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <proj-mates></proj-mates>
+    <login></login>
   </div>
 </template>
 
 <script>
 import ProjMates from './components/ProjMates.vue'
+import Login from './components/Login.vue'
+
 export default {
   name: 'App',
   components: {
-    'proj-mates' : ProjMates
+    'proj-mates' : ProjMates,
+    'login' : Login
   }
 }
 </script>
