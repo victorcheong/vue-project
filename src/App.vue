25e657dce6d6c21928ec5a22c31888ba48fdd956
Minzhi <li.minzhi@dhs.sg>
2020-03-20 20:37:40 +0800

    Edit Signup Interface
    Add dropdown menu and radio button for signup.vue
    Put some text in bold

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-   <!-- <proj-mates></proj-mates>  -->
-    <login></login>
+   <!-- <proj-mates></proj-mates>  
+    <login></login>  -->
+    <signup></signup>
   </div>
 </template>
 
 <script>
 // import ProjMates from './components/ProjMates.vue'
-import Login from './components/Login.vue'
+//import Login from './components/Login.vue'
+import Signup from './components/Signup.vue'
 
 export default {
   name: 'App',
   components: {
-    'login' : Login
+    //'login' : Login,
+    'signup': Signup
   }
 }
 </script>
