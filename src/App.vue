1147409f7d9c43b3cd532e113cc2b6c8d438ddf1
Minzhi <li.minzhi@dhs.sg>
2020-03-21 22:17:15 +0800

    Signup linked to firebase

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
   <div id="app">
-   <!-- <proj-mates></proj-mates>  
-    <login></login>  -->
+   <!-- <proj-mates></proj-mates>  -->
     <signup></signup>
   </div>
 </template>
 
 <script>
 // import ProjMates from './components/ProjMates.vue'
-//import Login from './components/Login.vue'
 import Signup from './components/Signup.vue'
 
 export default {
   name: 'App',
   components: {
-    //'login' : Login,
-    'signup': Signup
+    'signup' : Signup
   }
 }
 </script>
 
 <style scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import firebase from 'firebase';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJzuGdri_OLOX7hTZAdIUTzwceDVC3XWI",
+  authDomain: "bt3103-final-product.firebaseapp.com",
+  databaseURL: "https://bt3103-final-product.firebaseio.com",
+  projectId: "bt3103-final-product",
+  storageBucket: "bt3103-final-product.appspot.com",
+  messagingSenderId: "619318861214",
+  appId: "1:619318861214:web:ffa401fe7338cd7f91ca8f",
+  measurementId: "G-26YP3W344V"
+};
+  
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
